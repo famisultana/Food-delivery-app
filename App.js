bdf8f920a3c17d3f6572157319eb4a9b4ba6848a
Hassan Raza <36694573+iamhassanraza@users.noreply.github.com>
2019-10-27 16:25:56 +0500

    asda

--- App.js
+++ App.js
@@ -1,66 +1,62 @@
-import React from 'react';
-import { Button, View, Text ,TouchableWithoutFeedback} from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import Categories from './Screens/Categories';
-import Home from './Screens/Home';
-import SignIn from './Screens/SignIn';
-import EnterCode from './Screens/EnterCode';
-import SignUp from './Screens/SignUp';
-import Restaurants from './Screens/Restuarant';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import colors from './utilities/colors';
-import SplashScreen from './Screens/SplashScreen';
-import LocationBar from './Components/LocationBar';
-import GroceryList from './Screens/GroceryList';
+import React from "react";
+import { Button, View, Text, TouchableWithoutFeedback } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import Categories from "./Screens/Categories";
+import Home from "./Screens/Home";
+import SignIn from "./Screens/SignIn";
+import EnterCode from "./Screens/EnterCode";
+import SignUp from "./Screens/SignUp";
+import Restaurants from "./Screens/Restuarant";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import colors from "./utilities/colors";
+import SplashScreen from "./Screens/SplashScreen";
+import LocationBar from "./Components/LocationBar";
+import GroceryList from "./Screens/GroceryList";
 
 const RootStack = createStackNavigator(
   {
-    Splash:SplashScreen,
+    Splash: SplashScreen,
     SignIn: SignIn,
     SignUp: SignUp,
     EnterCode: EnterCode,
     Categories: Categories,
     MainScreen: Home,
-    GroceryList, GroceryList,
-    Restaurants:Restaurants
+    GroceryList,
+    GroceryList,
+    Restaurants: Restaurants
   },
   {
-    initialRouteName: 'SignIn',
+    initialRouteName: "SignIn",
 
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor: colors.secondary,
-        
+        backgroundColor: colors.secondary
       },
-      headerTintColor:colors.headerFont,
-      headerTitle:(
-        <LocationBar/>
-      ),
+      headerTintColor: colors.headerFont,
+      headerTitle: <LocationBar />,
       headerTitleStyle: {
-        fontWeight: 'bold',
-        alignSelf:'center',
-        justifyContent:'center'
+        fontWeight: "bold",
+        alignSelf: "center",
+        justifyContent: "center"
       },
       headerLeft: (
         <TouchableWithoutFeedback>
           <Icon
-          name="bars"
-          color={colors.headerFont}
-          style={{ fontSize: 20, marginLeft: 10 }}
-          onPress={() => alert('back')}
-        >
-        </Icon>
+            name="bars"
+            color={colors.headerFont}
+            style={{ fontSize: 20, marginLeft: 10 }}
+            onPress={() => alert("back")}
+          ></Icon>
         </TouchableWithoutFeedback>
       ),
       headerRight: (
         <TouchableWithoutFeedback>
           <Icon
-          name='shopping-cart'
-          color={colors.headerFont}
-          style={{ fontSize: 20, marginRight: 10 }}
-        >
-        </Icon>
+            name="shopping-cart"
+            color={colors.headerFont}
+            style={{ fontSize: 20, marginRight: 10 }}
+          ></Icon>
         </TouchableWithoutFeedback>
       )
     }
